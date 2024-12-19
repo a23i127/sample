79a3f13e79afdd3d9ba33ea80735bd9add46f3e1
a23i127 <a23i127ne@ous.jp>
2024-12-20 01:50:50 +0900

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by 高橋沙久哉 on 2024/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var kyouto: UIButton!
+    @IBOutlet weak var taka: UIButton!
+    @IBOutlet weak var oosaka: UIButton!
+    @IBOutlet weak var sinosaka: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
